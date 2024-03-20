84ab68a63dcec675bfb1220bfce1d6cc295044d8
ifUsernameIsTooLongNobodyWantToSeeItAll <whateveritbe@outlook.com>
2024-03-20 14:53:44 +0800

    Update

--- 20241/1/level2/level2.js
+++ 20241/1/level2/level2.js
@@ -3,11 +3,11 @@ const mul10 = (x) => x * 10
 const add100 = (x) => x + 100
 
 function compose(...functions) {
-     return function (x) {
-         return functions.reduceRight(
-             async (accumulator, currentValue) => currentValue(await accumulator), x
-         )
-     }
+    return function (x) {
+        return functions.reduceRight(
+            async (accumulator, currentValue) => currentValue(await accumulator), x
+        )
+    }
 }
 
 compose(add10, mul10, add100)(10).then(result => console.log(result))
