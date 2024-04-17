e320623ef51f825905f5be9e29bcf1b86e74bb72
ifUsernameIsTooLongNobodyWantToSeeItAll <149868847+ifUsernameIsTooLongNobodyWantToSeeItAll@users.noreply.github.com>
2024-04-17 23:42:32 +0800

    Finish Homework 5

--- 20241/5/2/1.jsx
+++ 20241/5/2/1.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-// 定义组件
 const first = () => <h2>Page 1</h2>;
 const second = () => <h2>Page 2</h2>;
 const third = () => <h2>Page 3</h2>;
 const error = () => <h2>Not Found</h2>;
 
-// App组件
 const App = () => {
   return (
     <Router>
